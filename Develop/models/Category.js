cd47e0c641030b2d061f89d6638fac3b306b1b2a
jmvega316 <jmvega316@gmail.com>
2022-09-21 12:49:45 -0400

    updated js files

---
+++ Develop/models/Category.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes, INTEGER } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    category_id:{
+      type: DataTypes.INTEGER,
+       allowNull: false,
+       primaryKey: true,
+       autoIncrement: true
+
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+
+    }
+    // define columns
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
